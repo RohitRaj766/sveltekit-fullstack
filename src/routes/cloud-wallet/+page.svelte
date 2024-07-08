2dd26456583a7a68d0447e02afb3a4463c6e0804
RohitRaj766 <rohitraj2k04@gmail.com>
2024-07-08 12:39:07 +0530

    .

---
+++ src/routes/cloud-wallet/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  /** @type {import('./$types').ActionData} */
+  export let form;
+  console.log("from :: ", form)
+
+</script>
+  
+  <div class="mt-10 pt-10 w-full max-w-xl p-12 mx-auto rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg">
+    <form method="POST" action="?/login" class="space-y-6" >
+      <div class="flex flex-wrap -mx-3 mb-2">
+        <div class="w-full px-3 mb-6">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="email">
+            Email
+          </label>
+          <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" placeholder="Enter email" name="email" required />
+        </div>
+        <div class="w-full px-3 mb-6">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="password">
+            Password
+          </label>
+          <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="password" type="password" placeholder="Enter password" name="password" required />
+        </div>
+      </div>
+      <div class="flex justify-end">
+        <button type="submit" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded tracking-widest">
+          LOGIN
+        </button>
+      </div>
+    </form>
+    {#if form?.success}
+    <p class="pt-2 text-black">Login successful. Your balance is {form.balance.data.balance.summary}</p>
+  {/if}
+  
+  </div>
+  
---
+++ src/routes/raw-wallet/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import { PrivateKey } from '@bsv/sdk'
+
+    let WIF:string
+    let address:string
+    let showKey = false;
+
+    function createKeys() {
+
+        const privKey = PrivateKey.fromRandom()
+        WIF = privKey.toWif()
+        console.log("Private Key IF format is ", WIF)
+        address = privKey.toAddress()
+        console.log("Your bitcoin address is ",address)
+        showKey = !showKey;
+
+    }
+
+
+</script>
+
+
+<section>
+    <button class="bg-yellow-500 hover:bg-blue-700 text-white font-bold mt-5 ml-2 px-2 rounded " on:click={createKeys}> Generate New Wallet Keys</button>
+    <p class="mt-4" class:fade={!showKey}  >Private Key WIF format is <span class="wif" id="wif">{WIF}</span></p>
+    <p class="mt-4" class:fade={!showKey}  >Your Bitcoin Address is <span class="wif" id="address">{address}</span></p>
+</section>
+<style>
+    .wif{
+  color: rgb(52, 18, 224); 
+}
+   .fade {
+    opacity: 0;
+    transition: opacity 0.5s ease-in-out;  }
+
+  .fade.true {
+    opacity: 1; 
+  }
+</style>
